9e7cf149acccf5907bb897df2ee4a5c037ca8c22
Jake Sarate <jakesa@Jakes-MacBook-Pro.local>
2021-03-28 05:28:58 -0700

    added CreateGameResponse and updated GameController to use it

--- src/main/kotlin/com/jake/sarate/definitelynotconnect4/controllers/GameController.kt
+++ src/main/kotlin/com/jake/sarate/definitelynotconnect4/controllers/GameController.kt
@@ -1,5 +1,8 @@
 package com.jake.sarate.definitelynotconnect4.controllers
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.util.JSONPObject
+import com.jake.sarate.definitelynotconnect4.models.CreateGameResponse
 import com.jake.sarate.definitelynotconnect4.models.GameInstance
 import com.jake.sarate.definitelynotconnect4.models.GameRequest
 
@@ -8,11 +11,18 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/drop_token")
-class GameController {
+class GameController() {
+
+    private val objectMapper = ObjectMapper()
 
     @PostMapping(consumes = ["application/json"], produces = ["application/json"])
     @ResponseStatus(HttpStatus.OK)
-    fun createGame(@RequestBody body: GameRequest): GameInstance {
-        return GameInstance()
+    fun createGame(@RequestBody body: GameRequest): CreateGameResponse {
+        val game = GameInstance(
+            columns = body.columns,
+            rows = body.rows,
+            players = body.players
+        )
+        return CreateGameResponse(game.gameId)
     }
 }
--- src/test/kotlin/com/jake/sarate/definitelynotconnect4/controllers/GameControllerTests.kt
+++ src/test/kotlin/com/jake/sarate/definitelynotconnect4/controllers/GameControllerTests.kt
@@ -1,13 +1,16 @@
 package com.jake.sarate.definitelynotconnect4.controllers
 
+import com.jake.sarate.definitelynotconnect4.models.CreateGameResponse
 import com.jake.sarate.definitelynotconnect4.models.GameInstance
 import com.jake.sarate.definitelynotconnect4.models.GameRequest
+import com.jake.sarate.definitelynotconnect4.models.GetGameResponse
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
@@ -23,11 +26,12 @@ class GameControllerTests {
     lateinit var restTemplate: TestRestTemplate
 
     @Test
-    fun shouldReturn200() {
+    fun creatingAGameShouldReturn200() {
         val request = HttpEntity(GameRequest(arrayOf("player 1", "player 2"), 4, 4))
-        val response = restTemplate.exchange("http://localhost:$port/api/drop_token", HttpMethod.POST , request, GameInstance::class.java)
+        val response = restTemplate.exchange("http://localhost:$port/api/drop_token", HttpMethod.POST , request, CreateGameResponse::class.java)
         val body = response.body
+        println(body)
         assertEquals(HttpStatus.OK, response.statusCode)
-        assertNotNull(body?.gameId)
+        assertNotNull(body)
     }
 }
