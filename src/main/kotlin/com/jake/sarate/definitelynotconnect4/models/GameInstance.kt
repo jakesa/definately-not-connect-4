d25799ce7a7e12a29e57d20eb95896a2e3746467
Jake Sarate <jakesa@Jakes-MacBook-Pro.local>
2021-03-28 04:12:13 -0700

    added the ability to create a game instance

---
+++ src/main/kotlin/com/jake/sarate/definitelynotconnect4/models/GameInstance.kt
@@ -0,0 +1,5 @@
+package com.jake.sarate.definitelynotconnect4.models
+
+import java.util.*
+
+data class GameInstance(val gameId: String = UUID.randomUUID().toString() )
---
+++ src/main/kotlin/com/jake/sarate/definitelynotconnect4/models/GameRequest.kt
@@ -0,0 +1,3 @@
+package com.jake.sarate.definitelynotconnect4.models
+
+data class GameRequest(val players: Array<String>, val columns: Int, val rows: Int)
